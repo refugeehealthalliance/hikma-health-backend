bb008c81deb6914369a5c234efdf8389c725a0a3
Sarah Rajab <sarah.rajab94@gmail.com>
2022-08-03 19:12:32 +0300

    add files from another clinic

--- app/config.py
+++ app/config.py
@@ -9,23 +9,23 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
     elif ENV == 'dev_docker':
         PG_HOST = 'db'
 
-    PG_USER = 'hikma_dev'
+    PG_USER = 'postgres'
     # PG_PASSWORD = 'password'
-    PG_PASSWORD = '96DDDDDB6425'
+    PG_PASSWORD = 'postgres'
     PG_DB = 'hikma_dev'
     FLASK_DEBUG = True
     PHOTOS_STORAGE_BUCKET = 'dev-api-photos'
-    EXPORTS_STORAGE_BUCKET = 'dev-api-exports'
+    EXPORTS_STORAGE_BUCKET = 'dppo-hikma-api-exports'
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
     DEFAULT_PROVIDER_ID_FOR_IMPORT = 'bd227f3d-0fbb-45c5-beed-8ce463481415'
 
 if ENV == 'prod':
     FLASK_DEBUG = False
-    PG_USER = os.environ['DB_NAME']
-    PG_PASSWORD = os.environ['DB_PASSWORD']
-    PG_HOST = 'localhost'
-    PG_DB = os.environ['DB_NAME']
-    PHOTOS_STORAGE_BUCKET = os.environ['PHOTOS_STORAGE_BUCKET']
-    EXPORTS_STORAGE_BUCKET = os.environ['EXPORTS_STORAGE_BUCKET']
+    PG_USER = 'kkrmagpvtdshhn'
+    PG_PASSWORD = '52fbbba8253619e5d1e0a2b76fbced6d45f4be24a692b272000c312659f8dad7'
+    PG_HOST = 'ec2-3-208-79-113.compute-1.amazonaws.com'
+    PG_DB = 'd78sid9s1m5u2l'
+    PHOTOS_STORAGE_BUCKET = 'dppo-hikma-photos-exports'
+    EXPORTS_STORAGE_BUCKET = 'dppo-hikma-api-exports'
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
     DEFAULT_PROVIDER_ID_FOR_IMPORT = os.environ['DEFAULT_PROVIDER_ID']
