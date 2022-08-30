fb677f52934bd71823d4cc9b9ab1ddea17ef7be9
Sarah Rajab <sarah.rajab94@gmail.com>
2022-08-30 23:14:12 +0300

    new build

--- app/config.py
+++ app/config.py
@@ -21,10 +21,10 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
 
 if ENV == 'prod':
     FLASK_DEBUG = False
-    PG_USER = "hikma_prod"
+    PG_USER = 'hikma_prod'
     PG_PASSWORD = os.environ['DB_PASSWORD']
     PG_HOST = os.environ['DB_HOST']
-    PG_DB = "hikma_prod"
+    PG_DB = 'hikma_prod'
     PHOTOS_STORAGE_BUCKET = os.environ['PHOTOS_STORAGE_BUCKET']
     EXPORTS_STORAGE_BUCKET = os.environ['EXPORTS_STORAGE_BUCKET']
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
