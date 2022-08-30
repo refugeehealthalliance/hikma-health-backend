166f93eb568508cad1271122466f81bf392e5ad8
sarah <sarah.m.rajab@gmail.com>
2022-08-30 20:09:44 +0300

    update config file

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
     PG_USER = 'postgres'
     # PG_PASSWORD = 'password'
     PG_PASSWORD = 'postgres'
-    PG_DB = 'hikma_dev'
+    PG_DB = 'rha-2'
     FLASK_DEBUG = True
     PHOTOS_STORAGE_BUCKET = 'dev-api-photos'
     EXPORTS_STORAGE_BUCKET = 'dev-api-exports'
@@ -21,10 +21,10 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
 
 if ENV == 'prod':
     FLASK_DEBUG = False
-    PG_USER = os.environ['DB_USER']
+    PG_USER = "hikma_prod"
     PG_PASSWORD = os.environ['DB_PASSWORD']
     PG_HOST = os.environ['DB_HOST']
-    PG_DB = os.environ['DB_NAME']
+    PG_DB = "hikma_prod"
     PHOTOS_STORAGE_BUCKET = os.environ['PHOTOS_STORAGE_BUCKET']
     EXPORTS_STORAGE_BUCKET = os.environ['EXPORTS_STORAGE_BUCKET']
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
